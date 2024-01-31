58786fe1253692f58b86a9b885c51fb143312d7f
ahmedzacki <ahmedxandulle01@gmail.com>
2024-01-31 16:30:57 -0500

    completed the UI

--- dist/comilers.js
+++ dist/comilers.js
@@ -1,3 +1,19 @@
-let message = "Hello World";
-console.log(message);
+"use strict";
+const textInput = document.getElementById("textInput");
+const textOutput = document.getElementById("textOutput");
+const submitBtn = document.getElementById("submitBtn");
+const resetBtn = document.getElementById("resetBtn");
+textOutput.readOnly = true;
+submitBtn === null || submitBtn === void 0 ? void 0 : submitBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    textOutput.value = textInput.value;
+    if (resetBtn)
+        resetBtn.style.display = "inline-block";
+});
+resetBtn === null || resetBtn === void 0 ? void 0 : resetBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    textInput.value = "";
+    textOutput.value = "";
+    resetBtn.style.display = "none";
+});
 //# sourceMappingURL=comilers.js.map
--- src/comilers.ts
+++ src/comilers.ts
@@ -1,2 +1,19 @@
-let message: string = "Hello World";
-console.log(message);
+const textInput = document.getElementById("textInput") as HTMLTextAreaElement;
+const textOutput = document.getElementById("textOutput") as HTMLTextAreaElement;
+const submitBtn = document.getElementById("submitBtn") as HTMLButtonElement;
+const resetBtn = document.getElementById("resetBtn") as HTMLButtonElement;
+
+textOutput.readOnly = true;
+
+submitBtn?.addEventListener("click", (event) => {
+  event.preventDefault();
+  textOutput.value = textInput.value;
+  if (resetBtn) resetBtn.style.display = "inline-block";
+});
+
+resetBtn?.addEventListener("click", (event) => {
+  event.preventDefault();
+  textInput.value = "";
+  textOutput.value = "";
+  resetBtn.style.display = "none";
+});
