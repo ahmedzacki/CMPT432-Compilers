57e5dcf3a8e5652e19a3df5c953ccf7df59bf310
ahmedzacki <ahmedxandulle01@gmail.com>
2024-02-19 18:38:15 -0500

    rewrote my compiler in java

--- src/Lexer.java
+++ src/Lexer.java
@@ -111,13 +111,10 @@ class Lexer {
     static {
         keywords = new HashMap<>();
         keywords.put("and", TokenType.AND);
-        keywords.put("class", TokenType.CLASS);
         keywords.put("else", TokenType.ELSE);
         keywords.put("false", TokenType.FALSE);
         keywords.put("for", TokenType.FOR);
-        keywords.put("fun", TokenType.FUN);
         keywords.put("if", TokenType.IF);
-        keywords.put("nil", TokenType.NIL);
         keywords.put("or", TokenType.OR);
         keywords.put("print", TokenType.PRINT);
         keywords.put("return", TokenType.RETURN);
